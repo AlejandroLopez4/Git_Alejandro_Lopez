ddf10666c6f7bf1966ace00fb5c1630e6169cde0
AlejandroLopez4 <alejandro.lopez3@live.u-tad.com>
2024-11-25 17:01:45 +0100

    "Modificacion 2"

--- src/alr/alr.java
+++ src/alr/alr.java
@@ -1,5 +1,9 @@
 package alr;
 
 public class alr {
-
+    public static void main(String[] args) {
+        System.out.println("Alumno: Alejandro López");
+        System.out.println("Primera Modificación desde Master");
+        System.out.println("Modificación desde Dev");
+    }
 }
